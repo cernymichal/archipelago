defd80acae2325bbeb323a6dce424af865891b69
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2021-10-05 18:44:05 +0200

    remove package scripts

--- install.sh
+++ install.sh
@@ -95,7 +95,7 @@ timedatectl set-ntp true
 
 # Pacstrap from arch repo
 echo -e "\n>Installing base and other packages through pacstrap\n"
-pacstrap $NEW_ROOT base base-devel linux linux-firmware xf86-input-libinput $MICROCODE $GRAPHICS_DRIVER $OPENGL $OPENGL32 neovim nano htop sudo networkmanager sxhkd go git grub efibootmgr python python-pip neofetch btrfs-progs grep xorg-xinit xorg gnome gnome-tweak-tool drawing
+pacstrap $NEW_ROOT base base-devel linux linux-firmware xf86-input-libinput $MICROCODE $GRAPHICS_DRIVER $OPENGL $OPENGL32 neovim nano htop networkmanager go git grub efibootmgr python python-pip neofetch btrfs-progs xorg-xinit xorg gnome gnome-tweak-tool drawing
 
 # Download locale and sudoers
 echo -e "\n>Downloading locale\n"
@@ -160,10 +160,6 @@ sudo -u $NEW_USER yadm clone --bootstrap -b master https://github.com/cernymicha
 
 # Link mirrorlist, pacman.conf, locale.gen and bashrc and copy sudoers
 sudo -u $NEW_USER /home/$NEW_USER/.local/bin/linkdots.sh
-
-# Download script for other packages
-sudo -u $NEW_USER curl -L https://raw.githubusercontent.com/cernymichal/archipelago/master/other_pkgs.sh > /home/$NEW_USER/other_pkgs.sh
-chmod +x /home/$NEW_USER/other_pkgs.sh
 
 # Enable services
 systemctl enable NetworkManager
--- other_pkgs.sh
+++
@@ -1,2 +0,0 @@
-pacman -Syu imagemagick redshift feh firefox vlc discord steam ffmpeg youtube-dl chromium pandoc wine blender ttf-fira-code
-yay -Syu steam-fonts opentabletdriver-git joplin-desktop steam-fonts adwaita-qt
