322d5bdad84454fc29a5cad476c425e81c4259e6
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2021-10-05 10:27:36 +0200

    change install.sh branch

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # archipelago
 
 ````shell
-curl -L https://raw.githubusercontent.com/cernymichal/archipelago/gnome/install.sh > install.sh
+curl -L https://raw.githubusercontent.com/cernymichal/archipelago/master/install.sh > install.sh
 chmod +x install.sh
 ./install.sh /mnt cz-qwertz cz Europe/Prague ARCHIPELAGO michal amd
 ```
--- other_pkgs.sh
+++ other_pkgs.sh
@@ -1,2 +1,2 @@
-pacman -Syu imagemagick redshift feh firefox vlc discord steam ffmpeg youtube-dl chromium pandoc wine blender
-yay -Syu steam-fonts opentabletdriver-git joplin-desktop steam-fonts
+pacman -Syu imagemagick redshift feh firefox vlc discord steam ffmpeg youtube-dl chromium pandoc wine blender ttf-fira-code
+yay -Syu steam-fonts opentabletdriver-git joplin-desktop steam-fonts adwaita-qt
